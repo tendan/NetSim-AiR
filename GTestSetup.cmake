094343df22e73282bdc6dcad2ce4975dc9bb2241
Maciej Ślusarz <maciej.slusarz@proton.me>
2023-11-30 15:39:23 +0100

    Initial files commit

---
+++ GTestSetup.cmake
@@ -0,0 +1,25 @@
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+)
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+enable_testing()
+
+set(TEST_DIR tests)
+set(SOURCE_TESTS_FILES
+)
+
+add_executable(
+        ${PROJECT_NAME}_Tests ${SOURCE_FILES} ${SOURCE_TESTS_FILES}
+)
+
+target_link_libraries(
+        ${PROJECT_NAME}_Tests GTest::gtest_main
+)
+
+include(GoogleTest)
+gtest_discover_tests(${PROJECT_NAME}_Tests)
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::cout << "Hello, NetSim!" << std::endl;
+
+    return 0;
+}
