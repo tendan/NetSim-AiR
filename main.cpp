75db3e14304472f6cdffe5fceb5e7c7b9267a410
Maciej Ślusarz <maciej.slusarz@proton.me>
2023-12-06 21:09:57 +0100

    My changes on storage_types.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "package.hpp"
+#include "types.hpp"
+#include "storage_types.hpp"
 
 int main() {
     std::cout << "Hello, NetSim!" << std::endl;
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,3 +1,19 @@
 //
 // Created by Oskar Słomion on 01/12/2023.
 //
+
+#include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    Package package;
+
+    if (this->package_queue_type_ == PackageQueueType::LIFO) {
+        package = std::move(this->package_list_.back());
+        this->package_list_.pop_back();
+    } else {
+        package = std::move(this->package_list_.front());
+        this->package_list_.pop_front();
+    }
+
+    return package;
+}
